381f601babf83c52dc9af288afd42d2c42cd664f
Alan Canales <alanyaco.cf@gmail.com>
2025-09-05 17:15:58 -0600

    read mcode file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+std::string readFile(const std::string& filePath)
+{
+    std::ifstream file(filePath);
+    if (!file.is_open()) {
+        std::cerr << "File could not be opened.\n";
+    }
+    std::string content;
+    std::getline(file, content);
+
+    return content;
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::string filePath = "../txts/mcode1.txt";
+    std::string content = readFile(filePath);
+    std::cout << "File content: " << content << std::endl;
     return 0;
 }
